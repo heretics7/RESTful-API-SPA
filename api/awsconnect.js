f30205a067de5c2b78fb35d26c494bfc69ef43d6
heretics7 <heretics7@naver.com>
2022-03-31 17:34:46 +0900

    노드 AWS DB 연결 성공

---
+++ api/awsconnect.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var awsconnect = express.Router();
+
+var mysql = require('mysql');
+var dbconfig = require('../db/config');
+
+var connection = mysql.createConnection(dbconfig);
+
+awsconnect.get('/', (req, res) => {
+    connection.query('SELECT * FROM interview', (error, result) => {
+        if(error) throw error;
+        console.log( 'DB 내용 :', result);
+        res.send(result);
+    })
+});
+
+module.exports = awsconnect;
--- api/router.js
+++ api/router.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var awsconnect = require('./awsconnect');
+
+router.use('/aws', awsconnect);
 
 router.get('/', (req, res) => {
     res.send('라우팅 연결 성공');
